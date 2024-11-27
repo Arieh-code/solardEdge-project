c073cbad905485e11df88e5baba48f09d9eda8b8
Arieh Norton <arieh.norton@gmail.com>
2024-11-27 16:03:56 +0200

    added view function

--- posts_project/posts/views.py
+++ posts_project/posts/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from .utils import fetch_posts
 
-# Create your views here.
+def post_list(request):
+    try:
+        posts = fetch_posts()
+    except Exception as e:
+        posts = []
+        print(f"Error fetching posts: {e}")  # Or log this error
+    return render(request, 'posts/post_list.html', {'posts': posts})
+
